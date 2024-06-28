7865167521fdf2073da5e7436b37df6c09141142
dharaaaaa528 <155510942+dharaaaaa528@users.noreply.github.com>
2024-06-28 22:29:00 +0800

    contentpage

--- major-project/contentpage2.php
+++ major-project/contentpage2.php
@@ -9,35 +9,35 @@
 <body>
     <?php include 'topnav.php'; ?>
     <div class="container">
-    	<div class="sidebar">
+        <div class="sidebar">
             <ul>
                 <li><a href="#">Profile</a></li>
                 <li><a href="contentpage.php">SQL techniques</a></li>
                 <li><a href="contentpage2.php">XSScript techniques</a></li>
                 
             </ul>
-     	</div>       
+        </div>
         <div class="content">
             <div class="technique" id="technique1">
-                <h2>SQL Technique 1</h2>
+                <h2>XS Script Technique 1</h2>
                 <p>[Description]</p>
                 <button>Attempt Quiz</button>
                 <button>Editor</button>
             </div>
             <div class="technique" id="technique2">
-                <h2>SQL Technique 2</h2>
+                <h2>XS Script Technique 2</h2>
                 <p>[Description]</p>
                 <button>Attempt Quiz</button>
                 <button>Editor</button>
             </div>
             <div class="technique" id="technique3">
-                <h2>SQL Technique 3</h2>
+                <h2>XS Script Technique 3</h2>
                 <p>[Description]</p>
                 <button>Attempt Quiz</button>
                 <button>Editor</button>
             </div>
             <div class="technique" id="technique4">
-                <h2>SQL Technique 4</h2>
+                <h2>XS Script Technique 4</h2>
                 <p>[Description]</p>
                 <button>Attempt Quiz</button>
                 <button>Editor</button>
