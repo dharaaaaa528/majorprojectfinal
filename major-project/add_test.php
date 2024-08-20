c9ba80d65d837d1b1b8927b0ae4af21a166d482f
dharaaaaa528 <155510942+dharaaaaa528@users.noreply.github.com>
2024-08-20 12:19:30 +0800

    add_test.php

--- major-project/add_test.php
+++ major-project/add_test.php
@@ -321,15 +321,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <input type="hidden" name="created_at" value="<?php echo date('Y-m-d H:i:s'); ?>">
             <input type="hidden" name="created_by" value="<?php echo htmlspecialchars($created_by_id); ?>">
             <input type="submit" value="Create Test">
-           <button class="back-button">
-                    <div class="icon">
-                        <svg width="25px" height="25px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
-                            <path fill="#000000" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"></path>
-                            <path fill="#000000" d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"></path>
-                        </svg>
-                    </div>
-                    
-                </button>
+           <button class="back-button" type="button" onclick="window.history.back();">
+    <div class="icon">
+        <svg width="25px" height="25px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#000000" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"></path>
+            <path fill="#000000" d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"></path>
+        </svg>
+    </div>
+</button>
+           
         </form>
     </div>
 </body>
